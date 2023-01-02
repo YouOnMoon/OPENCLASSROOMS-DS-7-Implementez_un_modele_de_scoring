75be0b48df22700c6ea86f6e69e51ceefc789f0e
Younes <e.younes@hotmail.fr>
2023-01-02 14:45:16 +0100

    Moifications train.py - export hyper-params au format .json et serialisation modèle format .joblib

--- src/train.py
+++ src/train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c86730329f1a71727c26e3fa41f8438fbbb8571b45e4886ebb045f532e5deee
-size 2872
+oid sha256:be6ba7b9502a3f7b30a4e00f4be15e315fa971f51d39833c68f661ef7ea3b434
+size 4573
