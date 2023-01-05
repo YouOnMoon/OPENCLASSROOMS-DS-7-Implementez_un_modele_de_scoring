f01fbf2f8607d6470da17f4f55ed2026099cb940
Younes <e.younes@hotmail.fr>
2023-01-05 19:38:17 +0100

    Finition des fonction de recherche des meilleurs hyper-paramtres et de sérialization des modèles au format .joblib

--- src/train.py
+++ src/train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be6ba7b9502a3f7b30a4e00f4be15e315fa971f51d39833c68f661ef7ea3b434
-size 4573
+oid sha256:97e21a131de1a19281fc9b94730cc9f1076ddae901bd7aff4855b9e7caec5244
+size 5025
