67c48196fdb94e43954969622690a91ff0a4bdb3
Younes <e.younes@hotmail.fr>
2023-01-07 09:39:15 +0100

    Finalisation mlflow tracking - debug log_model à mettre en place

--- src/train.py
+++ src/train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c2d00660fd099149899b41e3809b82dafea568ffa471a150bd80ed0097f6be8
-size 6829
+oid sha256:1b3385f896a9b39ef2ee1e1af801ed8b49defbe16ec47dcc22acedb19ffd1c75
+size 6934
