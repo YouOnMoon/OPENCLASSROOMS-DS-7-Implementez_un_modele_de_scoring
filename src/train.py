5c008f96c22fa394ffcd009d69d9942996767bc3
Younes <e.younes@hotmail.fr>
2023-01-07 07:32:56 +0100

    Mise en place process mlflow tracking pour le suivi des expériences - reste mlflow.log_model à ajouter

--- src/train.py
+++ src/train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:97e21a131de1a19281fc9b94730cc9f1076ddae901bd7aff4855b9e7caec5244
-size 5025
+oid sha256:1c2d00660fd099149899b41e3809b82dafea568ffa471a150bd80ed0097f6be8
+size 6829
