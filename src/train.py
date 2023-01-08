4f2c2a758a9d3a6f95e0c5582d2ff2eb9a2c6ff7
Younes <e.younes@hotmail.fr>
2023-01-08 07:34:34 +0100

    Modification fonction hyper_parameter tuning - ajout metrics accuracy/recall/f1/ROC_AUC et définition de nom des runs mlflow tracking

--- src/train.py
+++ src/train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b3385f896a9b39ef2ee1e1af801ed8b49defbe16ec47dcc22acedb19ffd1c75
-size 6934
+oid sha256:cf2a0a621c56410cf5c760e0ab0b18ec65f377c766092f8be9c2997a3de14ccc
+size 8341
