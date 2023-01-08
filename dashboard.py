1e759cb4c2e3d6111a9941f3c676697fe2ea2bf9
Younes <e.younes@hotmail.fr>
2023-01-08 09:22:22 +0100

    Démarrage mise en place tableau de board interactif

---
+++ dashboard.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e8e246fe692b336b736e5684adec871ddb3d718063bf2ea2a1d18a92aa6f6348
+size 606
---
+++ pages/page_2.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:befcc4746b581f0e3b6992250c285bc9281e04a6ba4ee8f01ec03cab77b2d667
+size 163
