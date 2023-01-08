18d87bff19e9413dd616af23277f29ec8975afdc
Younes <e.younes@hotmail.fr>
2023-01-08 12:10:25 +0100

    Ajout de la page de visualisation des données initiales au dashboard - finitions à mettre en place

--- pages/Data_Analysis.py
+++ pages/Data_Analysis.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd2bc61faf285a83a59165580a315844b0b82f71b9f324128cb5ecf17cc9b489
-size 1021
+oid sha256:69ce27bc966cb3013ab309b4c2799dea30508c40f4611f426e7f5dae6dacf6c8
+size 2485
