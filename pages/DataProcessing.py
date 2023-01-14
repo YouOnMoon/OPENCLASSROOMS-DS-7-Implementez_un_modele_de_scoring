ac67ef539285da8c27fdc279e3921abd2bef682e
Younes <e.younes@hotmail.fr>
2023-01-14 08:03:52 +0100

    Ajout gestion imbalanced classes avec SMOTE - Pipeline à mettre en place pour éviter overfitting

--- pages/DataProcessing.py
+++ pages/DataProcessing.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aefbd3e9dbcde7df336725b8a6bb425437fe306221567ce1c31e81bf4e764421
-size 3275
+oid sha256:888ac476c574f76ce3824290b9eae009e52193cfc3f793518e843d44e2e0e354
+size 3309
--- src/train.py
+++ src/train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74f319f3448aca70e7bc5f918a3caaeed1fef8ecaaed5c983688b58b07bf87a7
-size 8662
+oid sha256:fb1f04283fd48835be813e0066beccdf82d93c2a23edc3e9adc90f432145138c
+size 9093
