4e1c5a7f74a29267ea160fcbb0665128df08d4a5
Younes <e.younes@hotmail.fr>
2023-01-15 11:18:41 +0100

    Modification gestion Pipeline

--- src/train.py
+++ src/train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7aecdd50e6e3657feb4d374f422b6ad94ba4f329867aed99745fb5c0e6522ac7
-size 9132
+oid sha256:6573386c089d20b1b6b4b25b87ff850a60c5a75fbadb9f90361bfac31cf31d77
+size 9535
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74435f1e05e64042b1ed669bf0d49bc0d018b242491d0d347fd9065b554022f4
-size 9574
+oid sha256:c81b6dbacf685c54e33478c2866bdd4bc4f1e2a2b8f06294367ad0647fd0e289
+size 9589
