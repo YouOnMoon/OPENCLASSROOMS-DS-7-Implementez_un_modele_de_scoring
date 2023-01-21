2427c7ec76e983699bb154138f04a1ecd3ca4aa5
Younes <e.younes@hotmail.fr>
2023-01-21 06:47:26 +0100

    Debug partie sélection modèle et mise en place séléction expérience pour afichage des jeux de données pré-procéssés

--- src/train.py
+++ src/train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f7e6be53cf35836919450a6507060c3ed546b4e41650b4d7cf2df24370155e80
-size 9451
+oid sha256:b2ecaf44c86f078df0ae8d08cbc451eb11e3aef5436904b258ce9d5c1cf8343c
+size 11657
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ebb9fe9f3c9fedcced0d6496e7764766acf02a232c05ac67a755b1ff73ff66b
-size 9853
+oid sha256:8add2de2589dfaf0fedb8e0af94197254353cf0cd7b989c9c3151ec905dd91e9
+size 10004
