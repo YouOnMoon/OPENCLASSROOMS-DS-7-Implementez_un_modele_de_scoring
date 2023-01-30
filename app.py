d4813f6ce8cb84d7f6dc3a3aac9b7b1f84118e99
Younes <e.younes@hotmail.fr>
2023-01-30 23:16:04 +0100

    Debbug hyper_params tuning côté server

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:795c51dbc8f4ab4b55fa014d4c3dd00247a03a3e590f72663c56db16b6e1723b
-size 11150
+oid sha256:d7e0705a2f695e61abc92127c422ae61b5be8fd5d1fb51272e55c19217188bb1
+size 12336
--- src/train.py
+++ src/train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f37145e33b7e8f30ef374ae131940389802f8ab18791c482d40e9c59b3941c4
-size 14787
+oid sha256:65b7a0a71d2eebc69f396ea7cd18b409c105c9aa8390c2a2d29e801a5d5e186d
+size 15361
