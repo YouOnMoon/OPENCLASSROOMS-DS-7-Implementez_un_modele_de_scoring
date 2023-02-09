8eaf6fb4c8f546cff65eaa720c41a3acdef11293
Younes <e.younes@hotmail.fr>
2023-02-09 02:57:42 +0100

    Commit de référence - ajout partie lecture tableau html directement dans le dashboard

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:878f9c4ce19f6690cbb5ed2fc35f9e270ef982184d6625f6cf41125affceca70
-size 26038
+oid sha256:fec18463873353792669f8503f82a62828d7053ec8e741916a01a1c27ca6b4c0
+size 33233
--- pages/Production.py
+++ pages/Production.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6788a5ade870042db65fdbed4f55c21fdad01207b9bf6b032fe3a231d99bf6f
-size 3367
+oid sha256:4c1aba85485d08a51a8ce863f4ade4cb609ca247649bff051ab0e76db0937577
+size 10878
