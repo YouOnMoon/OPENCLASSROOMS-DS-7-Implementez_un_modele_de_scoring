29adb86857e71d2316bb66badf4c26b14f877433
Younes El <99097903+YouOnMoon@users.noreply.github.com>
2023-03-02 03:51:39 +0100

    Add files via upload

--- start.py
+++ start.py
@@ -1,3 +1,3 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5de6b3cc7cb28e04821c612520281b63029105c540f488652d67d1afdffa62e9
-size 60
+import os
+
+os.system("python -m uvicorn app:app --reload")
